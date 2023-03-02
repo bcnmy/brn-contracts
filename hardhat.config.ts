0e2fa973ce4a36b159d718fe41e04a58cb539135
Ankur Dubey <ankurdubey521@protonmail.com>
2023-03-02 16:09:13 +0530

    Migrate to hardhat-foundry from hardhat-preprocessor

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,7 @@
 import { HardhatUserConfig } from 'hardhat/config';
-import { execSync } from 'child_process';
 import '@typechain/hardhat';
-import 'hardhat-preprocessor';
 import '@nomiclabs/hardhat-ethers';
+import '@nomicfoundation/hardhat-foundry';
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -24,38 +23,6 @@ const config: HardhatUserConfig = {
       },
     ],
   },
-  preprocess: {
-    eachLine: (hre) => ({
-      transform: (line: string) => {
-        if (line.match(/^\s*import /i)) {
-          for (const [from, to] of getRemappings()) {
-            if (line.includes(from)) {
-              line = line.replace(from, to);
-              break;
-            }
-          }
-        }
-        return line;
-      },
-    }),
-  },
-  paths: {
-    sources: './src',
-    cache: './cache_hardhat',
-  },
 };
-
-let getRawRemappings = (): string => {
-  const remappings = execSync('forge remappings', { encoding: 'utf8' });
-  console.log(`Output of "forge remappings":\n${remappings}`);
-  getRawRemappings = () => remappings;
-  return remappings;
-};
-
-const getRemappings = () =>
-  getRawRemappings()
-    .split('\n')
-    .filter(Boolean) // remove empty lines
-    .map((line) => line.trim().split('='));
 
 export default config;
